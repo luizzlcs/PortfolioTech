0ef3b51a600d6a7873dda8cf6965f04941daefeb
Luiz Carlos <luizzlcs@gmail.com>
2025-03-19 21:25:34 -0300

    Contantes de images e links

---
+++ lib/src/constants/app_image.dart
@@ -0,0 +1,17 @@
+abstract class AppImage{
+  static const String imageProfile = 'assets/images/luizzlcs.png';
+  static const String gitHub = 'assets/images/github.png';
+  static const String linkedin = 'assets/images/linkedin.png';
+  static const String whatsApp = 'assets/images/whatsapp.png';
+  static const String email = 'assets/images/email.png';
+  static const String sincofarma = 'assets/images/sincofarma.png';
+  static const String diretorioDeLinks = 'assets/images/diretorioDeLinks.png';
+  static const String sevenManager = 'assets/images/sevenManager.png';
+  static const String administrativoIASD = 'assets/images/administrativoIASD.png';
+  static const String campoMinado = 'assets/images/campoMinado.png';
+  static const String calculadora = 'assets/images/calculadora.png';
+  static const String dsCommerce = 'assets/images/dscommerce.png';
+  static const String dsEvent = 'assets/images/dsevent.png';
+  static const String dsClient = 'assets/images/dsclient.png';
+
+}
---
+++ lib/src/constants/app_links.dart
@@ -0,0 +1,7 @@
+abstract class AppLinks {
+  static const String gitHub = 'https://github.com/luizzlcs';
+  static const String linkedin = 'https://linkedin.com/in/luizzlcs';
+  static const String whatsApp = 'https://wa.me/5584988369729';
+  static const String email = 'mailto:luizzlcs@gmail.com';
+ 
+}
