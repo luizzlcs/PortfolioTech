ef2fd7653e01ae6957a7409ace7bfc8c6e3e76aa
Luiz Carlos <luizzlcs@gmail.com>
2025-03-31 11:52:19 -0300

    Atualizado link de arquivos do Notion para o Firebase

--- lib/src/constants/app_links.dart
+++ lib/src/constants/app_links.dart
@@ -3,9 +3,9 @@ abstract class AppLinks {
   static const String linkedin = 'https://linkedin.com/in/luizzlcs';
   static const String whatsApp = 'https://api.whatsapp.com/send/?phone=5584988369729&text=Oi+Luiz+Carlos%21+Tudo+bem%3F+Estou+entrando+em+contato+porque+seu+perfil+se+encaixa+em+uma+oportunidade+que+temos.+Podemos+conversar%3F&type=phone_number&app_absent=0';
   static const String email = 'mailto:luizzlcs@gmail.com';
-  static const String curriculo = 'https://file.notion.so/f/f/a3adf51a-bad3-4c29-890a-100bf6e25e65/553c8ae6-c331-4853-b1fb-0a11881c4155/Currculo_2025_.pdf?table=block&id=1c7e2e57-f164-80a2-a983-d668a23d63c4&spaceId=a3adf51a-bad3-4c29-890a-100bf6e25e65&expirationTimestamp=1743451200000&signature=5R5nUTLQL_FES-611O9pwibWlLM8dhuR--WcHpQrpJU&downloadName=Curr%C3%ADculo_2025+.pdf';
-  static const String senacDevelopedJava = 'https://file.notion.so/f/f/a3adf51a-bad3-4c29-890a-100bf6e25e65/22ae227f-fb2b-4492-9864-95600e17d343/Programador_Java.pdf?table=block&id=1b2e2e57-f164-80d9-92a1-e0594cb5a637&spaceId=a3adf51a-bad3-4c29-890a-100bf6e25e65&expirationTimestamp=1743379200000&signature=QbTYiIKizTT9rnvtKJakbAAzgo5wfKw80WkhZDVxK1c&downloadName=Programador+Java.pdf';
-  static const String academiaDoFlutterDartWeek2023 = 'https://file.notion.so/f/f/a3adf51a-bad3-4c29-890a-100bf6e25e65/dc7bf144-c553-49b0-83f1-6785d8a330b8/CertificadoDartWeek11.pdf?table=block&id=1c1e2e57-f164-800b-bf84-ecc429b4a9e3&spaceId=a3adf51a-bad3-4c29-890a-100bf6e25e65&expirationTimestamp=1743379200000&signature=NRSkTQAsIGlKRBBfRlQhR0Olo8Ak9_DzQYvYOsbV7lM&downloadName=CertificadoDartWeek11.pdf';
+  static const String curriculo = 'https://firebasestorage.googleapis.com/v0/b/seven-manager-cffc7.appspot.com/o/Documentos%2FCurr%C3%ADculo_2025%20.pdf?alt=media&token=573e1534-3f78-495f-95a1-18f1937c16c1';
+  static const String senacDevelopedJava = 'https://firebasestorage.googleapis.com/v0/b/seven-manager-cffc7.appspot.com/o/Documentos%2FProgramador%20Java.pdf?alt=media&token=95f81a62-d37d-454a-8d11-dcc740a8e9b5';
+  static const String academiaDoFlutterDartWeek2023 = 'https://firebasestorage.googleapis.com/v0/b/seven-manager-cffc7.appspot.com/o/Documentos%2FCertificadoDartWeek11.pdf?alt=media&token=9e5eaa26-9d7c-4032-b62e-8a6a2a9fe69f';
   static const String devSuperiorJavaSpringProfessional = 'https://devsuperior.club/c/5-5711';
   static const String flutterMasterClass = 'https://ll-app-certificates.s3.sa-east-1.amazonaws.com/89685a45-ad9c-47ab-86e8-31b9bd4bd70d.png';
  
